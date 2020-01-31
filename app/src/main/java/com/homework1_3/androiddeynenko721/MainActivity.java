ada708f8cdd597ccef1b45554c594bab458d2ef9
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-01-31 15:32:41 +0300

    Initial commit

---
+++ app/src/main/java/com/homework1_3/androiddeynenko721/MainActivity.java
@@ -0,0 +1,43 @@
+package com.homework1_3.androiddeynenko721;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Uri uri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText coordinates = findViewById(R.id.editGM);
+        final String query = coordinates.getText().toString();
+        Button btnSync = findViewById(R.id.button);
+        btnSync.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                if(Character.isLetter(0)){
+                    uri = Uri.parse(getString(R.string.geoAddressGM,query));
+                    intent.setData(uri);
+                    startActivity(intent);
+                } else {
+                    uri = Uri.parse(getString(R.string.geoCoordinatesGM, query));
+                    intent.setData(uri);
+                    startActivity(intent);
+                }
+
+
+
+            }
+        });
+    }
+}
